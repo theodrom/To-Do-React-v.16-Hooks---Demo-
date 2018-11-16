942eea1a75b5041031304b80f622685b33934d25
Theo <theodrom@users.noreply.github.com>
2018-11-16 16:24:01 +0100

    final commit

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 
 # To-Do demo
 
-This is a demo-app based on React 16.7 alpha version with **Hooks**  
+This is a demo-app based on React 16.7 _alpha version with **Hooks**.  
 
-Hooks are functions that let you “hook into” React state and lifecycle features from function components. Hooks don’t work inside classes — they let you use React without classes.
+**Hooks** are functions that let us “hook into” React state and lifecycle features from function components. **Hooks** don’t work inside classes — they let us use React without classes.  
+In this demo, we are using only the **useState()** hook. The **useState()** returns a pair of values: the current state value (todos) and a function (setTodos) that lets us update state.  
+
+The demo is based on this tutorial: <https://scotch.io/tutorials/build-a-react-to-do-app-with-react-hooks-no-class-components?utm_source=newsletter&utm_medium=email&utm_campaign=build_a_crud_app_with_react_hooks_implement_smooth_scrolling_in_react&utm_term=2018-11-14>
--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
+
+// component(cmpt) that we pass in a (todo) and it returns the text part 
+const Todo = ({ todo, index, completeTodo, removeTodo }) => {
+  return (
+    <div className="todo"
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}>
+    {todo.text}
+
+    <div>
+    <button onClick={() => completeTodo(index)}>Complete</button>
+    <button onClick={() => removeTodo(index)}>x</button>
+    </div>
+    </div>
+  );
+} 
 
 export default function App() {
 
-  // destructuring the array with todos and setTodos
-  // useState returns a pair: the current state value and a function that lets you update it.
+  // The destructuring assignment syntax is a JavaScript expression 
+  // that makes it possible to unpack values from arrays, 
+  // or properties from objects, into distinct variables, based on their indexes.
+  
+  // here we are destructuring the array declaring variables todos and setTodos
+  // The only argument to the useState() Hook is the initial state. Unlike with classes, the state doesn’t have to be an object. 
+  // We can keep a number or a string if that’s all we need
+  // useState returns a pair of values: the current state value (todos) and a function (setTodos) that lets you update it.
   const [todos, setTodos] = useState([
-    { text: "Learn about React" },
-    { text: "Meet friend for lunch" },
-    { text: "Build really cool todo app" }
+    {
+      text: "Learn about React",
+      isCompleted: false
+    },
+    {
+      text: "Meet friend for lunch",
+      isCompleted: false
+    },
+    {
+      text: "Build really cool todo app",
+      isCompleted: false
+    }
   ]);
 
   const addTodo = text => {
-    // check the spread operator...
+   // Spread syntax allows an iterable such as an array expression or string 
+   // to be expanded in places where zero or more arguments (for function calls) 
+   // or elements (for array literals) are expected, or an object expression to be expanded in places 
+   // where zero or more key-value pairs (for object literals) are expected.
+   // Here we spread the todos object array
     // we pass in the key word {text} to set in the new todo
     const newTodos = [...todos, { text }];
+    // the setTodos() is a function initialzed by the useState 'hook' from above
+    // and takes the new state value as an argument.
     setTodos(newTodos);
   };
 
-  // component(cmpt) that we pass in a (todo) and it returns the text part 
-  const Todo = ({ todo }) => <div className="todo">{todo.text}</div>;
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    // added a toggle complete function
+    newTodos[index].isCompleted = !todos[index].isCompleted;;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
 
   // we'll render our todos here ...
   return (
@@ -33,10 +79,12 @@ export default function App() {
             key={index}
             index={index}
             todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
           />
         ))}
+        <TodoForm addTodo={addTodo} />
       </div>
-      <TodoForm addTodo={addTodo} />
     </div>
   );
 }
@@ -65,7 +113,6 @@ function TodoForm({addTodo}) {
         value={value}
         onChange={e => setValue(e.target.value)}
       />
-
     </form>
   );
 }
